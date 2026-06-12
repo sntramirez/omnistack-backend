fe6963bd365448a9c6d9f14b25ff98742a7ada28
Ramirez Vilaña Santiago Patricio <spramirezv@corporaciongpf.com>
2026-06-12 07:36:31 -0500

    Fix `subcategory_code` mapping and adjust ordering in `rms-items.sql` for accurate and consistent query results.

--- src/main/resources/sql/business-lines/oracle/rms-items.sql
+++ src/main/resources/sql/business-lines/oracle/rms-items.sql
@@ -1,15 +1,15 @@
 SELECT
-    TRIM(im.ITEM)           AS rms_item_code,
-    TO_CHAR(c.CLASS)        AS category_code,
-    c.CLASS_NAME            AS category_name,
-    TO_CHAR(sc.SUBCLASS_ID) AS subcategory_code,
-    sc.SUB_NAME             AS subcategory_name,
-    im.ITEM_DESC            AS description
+    TRIM(im.ITEM)        AS rms_item_code,
+    TO_CHAR(c.CLASS)     AS category_code,
+    c.CLASS_NAME         AS category_name,
+    TO_CHAR(sc.SUBCLASS) AS subcategory_code,
+    sc.SUB_NAME          AS subcategory_name,
+    im.ITEM_DESC         AS description
 FROM ITEM_MASTER im
 JOIN CLASS c
     ON c.CLASS = im.CLASS
 JOIN SUBCLASS sc
-    ON sc.CLASS       = im.CLASS
-   AND sc.SUBCLASS_ID = im.SUBCLASS
+    ON sc.CLASS    = im.CLASS
+   AND sc.SUBCLASS = im.SUBCLASS
 WHERE TRIM(im.ITEM) IN (:rms_item_codes)
-ORDER BY TO_CHAR(c.CLASS), TO_CHAR(sc.SUBCLASS_ID), TRIM(im.ITEM)
+ORDER BY c.CLASS, sc.SUBCLASS, TRIM(im.ITEM)
